cda13dc75526cfb78c0be7ee24e7581b02c4f8a7
Teeratus Rojananak <t0861246000@hotmail.com>
2026-09-08 17:00:03 +0700

    uncomment for test toggle on-off

--- network/Win10-Advanced_sharing_settings.ps1
+++ network/Win10-Advanced_sharing_settings.ps1
@@ -66,11 +66,11 @@ Set-ItemProperty -Path $msvPath -Name "NtlmMinServerSec" -Value 0 -Type DWord
 
 # -Password protected sharing
 # --Turn on password protected sharing
-#$guest = Get-LocalUser | Where-Object { $_.SID -like "*-501" }
-#net user $guest.Name /active:no
-#Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LimitBlankPasswordUse" -Value 1 -Type DWord
-#Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "everyoneincludesanonymous" -Value 0 -Type DWord
-#Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "RestrictNullSessAccess" -Value 1 -Type DWord
+$guest = Get-LocalUser | Where-Object { $_.SID -like "*-501" }
+net user $guest.Name /active:no
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "LimitBlankPasswordUse" -Value 1 -Type DWord
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name "everyoneincludesanonymous" -Value 0 -Type DWord
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "RestrictNullSessAccess" -Value 1 -Type DWord
 #
 # --Turn off password protected sharing
 $guest = Get-LocalUser | Where-Object { $_.SID -like "*-501" }
